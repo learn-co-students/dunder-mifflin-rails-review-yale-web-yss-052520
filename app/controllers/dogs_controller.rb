5bc3dff94519223af942606831ba7a5d88f5735a
zayzay500 <lindsay.pierce@yale.edu>
2020-06-24 15:23:29 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,26 @@
 class DogsController < ApplicationController
+
+    before_action :get_dog, only: :show
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def sorted_index
+        @dogs = Dog.all.sort_by{|dog| dog.employee_count}.reverse
+    end
+    
+    def show
+    end
+
+    private
+
+    def get_dog
+        @dog = Dog.find(params[:id])
+    end
+    
+    # def post_params(*args)
+    #     params.require(:post).permit(*args)
+    # end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+    # validates :name, presence: true
+    # # validates :age, inclusion: 0...30
+    # validates :age, numericality: { only_integer: true, greater_than: 0, less_than: 30}
+    def to_s
+        first_name + " " + last_name 
+    end
 end
