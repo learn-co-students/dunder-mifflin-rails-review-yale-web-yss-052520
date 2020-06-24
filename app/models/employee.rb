7c34a2ffe6c1b14daff971fb0b528c1f6323cb81
nicolewang2000 <nicole.wang@yale.edu>
2020-06-24 15:50:29 -0400

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
     validates :alias, uniqueness: true
     validates :title, uniqueness: true
     belongs_to :dog
+
+    def full_name
+        self.first_name + " "+ self.last_name
+    end 
 end
