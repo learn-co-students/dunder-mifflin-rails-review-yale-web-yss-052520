63a7a56123b6cc9d9f3f5de103efbe2736e5d86d
nicolewang2000 <nicole.wang@yale.edu>
2020-06-24 15:34:01 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def sort
+        @dogs = Dog.all.sort_by{|dog|dog.employees.length}
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dogs/sort', to: "dogs#sort"
+   post '/dogs/sort', to: "dogs#sort"
 end
